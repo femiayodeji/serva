d4d33124e52845a6c29c64dbd8913d599a7c3f13
Instincts <feminstincts@gmail.com>
2020-05-02 22:45:37 +0100

    refactored code

--- src/main.rs
+++ src/main.rs
@@ -21,19 +21,16 @@ fn handle_connection(mut stream: TcpStream){
 
     let get = b"GET / HTTP/1.1\r\n";
 
-    if buffer.starts_with(get){
-        let contents = fs::read_to_string("hello.html").unwrap();
-
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-    
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();    
+    let (status_line, filename) = if buffer.starts_with(get){
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-        let contents = fs::read_to_string("404.html").unwrap();
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
-        let response = format!("{}{}", status_line, contents);
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
