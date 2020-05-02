c7b9e1f07c5397be789581293673dbf48cccbe24
Instincts <feminstincts@gmail.com>
2020-05-02 22:22:58 +0100

    add returning real html

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use std::fs;
 
 fn main() {
     println!("Serva");
@@ -16,8 +17,12 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream){
     let mut buffer = [0; 512];
-
     stream.read(&mut buffer).unwrap();
-    
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let contents = fs::read_to_string("hello.html").unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
