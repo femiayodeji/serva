27a3d34412548f6e5c71f02b11759d025b364815
Instincts <feminstincts@gmail.com>
2020-05-02 22:12:25 +0100

    add handle connection function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
 use std::net::TcpListener;
 
 fn main() {
     println!("Serva");
+
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming(){
         let stream = stream.unwrap();
 
-        println!("Connction established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+    
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
