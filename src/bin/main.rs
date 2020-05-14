364b1d78e3d2129ca714b5e10cc2144012000ef3
Femi Ayodeji <feminstincts@gmail.com>
2020-05-14 20:54:05 +0100

    created library for threadpool

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,16 +4,18 @@ use std::net::TcpListener;
 use std::fs;
 use std::thread;
 use std::time::Duration;
+use serva::ThreadPool;
 
 fn main() {
     println!("Serva");
 
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming(){
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
